014a922b3131f11e9e698293eea4a7ebdc473e00
Dean Gaffney <deanpatrickgaffney@gmail.com>
2021-11-06 14:36:40 +0000

    updated dependencies, added cli util and dry run support

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
-require('dotenv').config()
-
+require('dotenv').config();
+const fs = require('fs');
+const winston = require('winston');
+const nodemailer = require('nodemailer');
+const { Command } = require('commander');
 const emailUtils = require('./utils/email-utils');
-var nodemailer = require('nodemailer');
 
-var users = require('./data/users.json');
-var remainingUsers = [...users];
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'combined.log' }),
+  ],
+});
 
-var transporter = emailUtils.createTransporter(nodemailer);
+const program = new Command();
 
-emailUtils.sendEmails(transporter, users, remainingUsers);
+program
+  .requiredOption('-u, --users-file <file>', 'Required, the json file containing the users')
+  .option('-d, --dry-run', 'Optional, if provided the emails will not be sent, but the user pairs will be shown');
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+const { usersFile: usersFilePath, dryRun: isDryRunEnabled } = options;
+
+if (!usersFilePath) {
+  logger.error('Users file path was not provided. --users-file is required.');
+  process.exit(1);
+}
+
+if (!fs.existsSync(usersFilePath)) {
+  logger.error(`File path "${usersFilePath}" does not exist`);
+  process.exit(1);
+}
+
+if (isDryRunEnabled) {
+  logger.info('Dry run is ENABLED');
+}
+
+const users = JSON.parse(fs.readFileSync(usersFilePath));
+
+const remainingUsers = [...users];
+
+const transporter = emailUtils.createTransporter(nodemailer);
+
+emailUtils.sendEmails({
+  transporter, users, remainingUsers, logger, isDryRunEnabled
+});
--- utils/email-utils.js
+++ utils/email-utils.js
@@ -1,26 +1,32 @@
 const path = require('path');
-const winston = require('winston');
-const logger = winston.createLogger({
-    level: 'info',
-    format: winston.format.json(),
-    transports:[
-        new winston.transports.File({ filename: 'error.log', level: 'error'}),
-        new winston.transports.File({ filename: 'combined.log'})
-    ]
-});
 
 /**
- * Sends emails to the users with their options
- * @param {object} transporter the transporter to use for sending the emails
- * @param {array} users the users to send the emails to
- * @param {array} remainingUsers the remaining users to select from
+ * Returns the subject for the email
  */
-function sendEmails(transporter, users, remainingUsers) {
-    logger.info('Generated Users with Options:\n');
-    generateUserOptions(users, remainingUsers).forEach((user) => {
-        logger.info(user);
-        sendEmail(transporter, getEmailOptions(user));
-    });
+function getEmailSubject() {
+  return 'Secret Santa';
+}
+
+/**
+ * Returns html for the email
+ * @param {object} user the user object to use in the html
+ */
+function getEmailHtml(user) {
+  return `<h1>Secret Santa!</h1>
+  <p>Ho Ho Ho!! Your secret santa this year is <strong>${user.option.name}</strong></p>
+  <img src="cid:${user.option.email}"/>
+  `;
+}
+
+function getAttachments(user) {
+  const filePath = path.join(__dirname, `../images/${user.option.name}.jpg`);
+  return [
+    {
+      filename: `${user.option.name}.jpg`,
+      path: filePath,
+      cid: `${user.option.email}`,
+    },
+  ];
 }
 
 /**
@@ -28,25 +34,28 @@ function sendEmails(transporter, users, remainingUsers) {
  * @param {object} transporter the nodemailer transporter for sending the emails
  * @param {object} mailOptions the options to use for sending the email
  */
-function sendEmail(transporter, mailOptions){
-    transporter.sendMail(mailOptions, (err, info) => {
-        if (err) {
-            logger.error(err);
-        } else {
-            logger.info(info);
-        }
-    });
+function sendEmail(transporter, mailOptions, logger) {
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      logger.error(err);
+    } else {
+      logger.info(info);
+    }
+  });
 }
 
 /**
- * Generate the users options
- * @param {array} users the users to generate options for
- * @param {array} remainingUsers the remaining users left to choose from
+ * Gets the email details for the supplied user
+ * @param {object} user the user object to create the email options from
  */
-function generateUserOptions(users, remainingUsers) {
-    return users.map((user) => {
-        return { ...user, option: getRandomUser(user, remainingUsers) };
-    });
+function getEmailOptions(user) {
+  return {
+    from: process.env.EMAIL_ADDRESS,
+    to: user.email,
+    subject: getEmailSubject(),
+    html: getEmailHtml(user),
+    attachments: getAttachments(user),
+  };
 }
 
 /**
@@ -55,75 +64,70 @@ function generateUserOptions(users, remainingUsers) {
  * @param {array} remainingUsers the remainingUsers array
  */
 function getRandomUser(user, remainingUsers) {
-    let randUser = user;
-    do {
-        randUser = remainingUsers[Math.floor(Math.random() * remainingUsers.length)];
-    } while (randUser.name === user.name);
+  let randUser = user;
+  do {
+    randUser = remainingUsers[Math.floor(Math.random() * remainingUsers.length)];
+  } while (randUser.name === user.name);
 
-    const index = remainingUsers.indexOf(randUser);
-    remainingUsers.splice(index, 1);
-    return randUser;
+  const index = remainingUsers.indexOf(randUser);
+  remainingUsers.splice(index, 1);
+  return randUser;
+}
+
+/**
+ * Generate the users options
+ * @param {array} users the users to generate options for
+ * @param {array} remainingUsers the remaining users left to choose from
+ */
+function generateUserOptions(users, remainingUsers) {
+  return users.map((user) => ({ ...user, option: getRandomUser(user, remainingUsers) }));
+}
+
+/**
+ * Sends emails to the users with their options
+ * @param {object} transporter the transporter to use for sending the emails
+ * @param {array} users the users to send the emails to
+ * @param {array} remainingUsers the remaining users to select from
+ * @param {object} logger the logger
+ * @param {boolean} isDryRunEnabled if dry run is enabled
+ */
+function sendEmails({
+  transporter, users, remainingUsers, logger, isDryRunEnabled = false
+}) {
+  logger.info('Generated Users with Options:\n');
+  const generatedUsers = generateUserOptions(users, remainingUsers);
+  generatedUsers.forEach((user) => {
+    logger.info(user);
+    if (isDryRunEnabled === false) {
+      sendEmail(transporter, getEmailOptions(user));
+    } else {
+      logger.info('Dry run is enabled - skipping');
+    }
+  });
+  logger.info(generatedUsers);
 }
 
 /**
  * Gets a transporter for sending the emails
  */
 function createTransporter(nodemailer) {
-    return nodemailer.createTransport(
-        `smtps://${encodeURIComponent(process.env.EMAIL_ADDRESS)}:${process.env
-            .EMAIL_PASSWORD}@smtp.gmail.com`
-    );
-}
-
-/**
- * Gets the email details for the supplied user
- * @param {object} user the user object to create the email options from
- */
-function getEmailOptions(user) {
-    return {
-        from: process.env.EMAIL_ADDRESS,
-        to: user.email,
-        subject: getEmailSubject(),
-        html: getEmailHtml(user),
-        attachments: getAttachments(user)
-    };
-}
-
-/**
- * Returns the subject for the email
- */
-function getEmailSubject(){
-    return 'Secret Santa';
-}
-
-/**
- * Returns html for the email
- * @param {object} user the user object to use in the html
- */
-function getEmailHtml(user) {
-    return `<h1>Secret Santa!</h1>
-    <p>Ho Ho Ho!! Your secret santa this year is <strong>${user.option.name}</strong></p>
-    <img src="cid:${user.option.email}"/>
-    `
-}
-
-function getAttachments(user) {
-    const filePath = path.join(__dirname, `../images/${user.option.name}.jpg`);
-    return [{
-        filename: `${user.option.name}.jpg`,
-        path: filePath,
-        cid: `${user.option.email}`
-    }]
+  return nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
 }
 
 module.exports = {
-    sendEmails: sendEmails,
-    generateUserOptions: generateUserOptions,
-    getRandomUser: getRandomUser,
-    createTransporter: createTransporter,
-    getEmailOptions: getEmailOptions,
-    getEmailSubject: getEmailSubject,
-    getEmailHtml: getEmailHtml,
-    getAttachments,
-    sendEmail: sendEmail
+  sendEmails,
+  generateUserOptions,
+  getRandomUser,
+  createTransporter,
+  getEmailOptions,
+  getEmailSubject,
+  getEmailHtml,
+  getAttachments,
+  sendEmail,
 };
